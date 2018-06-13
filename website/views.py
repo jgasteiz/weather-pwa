4af1e287627ef4d676223ae05130b1a241d4a377
Javi Manzano <javi.manzano.oller@gmail.com>
2018-06-13 18:43:59 +0100

    Simplify the index view

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
+import json
 import os
 
 import requests
-from django.http import HttpResponse
+from django.shortcuts import render
 
 OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org'
 WEATHER_ENDPOINT = 'data/2.5/weather?q={query}&appid={app_id}'
@@ -9,7 +10,8 @@ APP_ID = os.environ.get('APP_ID')
 
 
 def index(request):
-    query = request.GET.get('q', 'London,uk')
+    query = request.GET.get('q', 'London,UK')
     weather_endpoint = WEATHER_ENDPOINT.format(query=query, app_id=APP_ID)
-    r = requests.get('%s/%s' % (OPEN_WEATHER_MAP_URL, weather_endpoint))
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    res = requests.get('%s/%s' % (OPEN_WEATHER_MAP_URL, weather_endpoint))
+    pretty_response = json.dumps(res.json(), sort_keys=True, indent=4)
+    return render(request, 'index.html', context={'weather_data': pretty_response})
