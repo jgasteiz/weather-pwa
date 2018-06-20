c630a62d0dbcd5b6e2a75ac86dc685d42c852ce5
Javi Manzano <javi.manzano.oller@gmail.com>
2018-06-20 22:49:49 +0100

    Add a service worker with a “stale while revalidate” caching strategy #3

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.cache import cache_control
+from django.views.generic import TemplateView
 
 from website import api
 from website import tasks
@@ -14,5 +16,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/forecast', api.forecast, name='forecast'),
     url(r'^tasks/forecast', tasks.fetch_forecast, name='fetch_forecast'),
+    url(r'^service-worker.js', cache_control(max_age=2592000)(TemplateView.as_view(
+        template_name="service-worker.js",
+        content_type='application/javascript',
+    )), name='service-worker.js'),
     path('admin/', admin.site.urls),
 ]
