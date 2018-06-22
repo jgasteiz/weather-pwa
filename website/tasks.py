91c241c538df8c15d24345ad6cb38a281727088e
Javi Manzano <javi.manzano.oller@gmail.com>
2018-06-22 20:49:34 +0100

    Delete unused tasks module #1

--- website/tasks.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-from weatherservice.controller import AccuWeatherController
-
-
-def fetch_forecast(request):
-    forecast_controller = AccuWeatherController()
-    forecast_controller.fetch_forecast()
-    return HttpResponse('Ok')
