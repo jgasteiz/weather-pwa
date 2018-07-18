e0bbb71e8a0cc9bee09823d351ded9c1132308b7
Javi Manzano <javi.manzano.oller@gmail.com>
2018-07-18 20:24:35 +0100

    Replace the WeatherContainer tests with e2e tests

--- website/static/app/containers/WeatherContainer.spec.js
+++ website/static/app/containers/WeatherContainer.spec.js
@@ -1,132 +1,138 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk'
+import {createStore,  compose, applyMiddleware} from 'redux';
 import {MemoryRouter} from 'react-router';
-import {WeatherContainer} from './WeatherContainer';
-import AboutPage from '../components/AboutPage';
+import appReducers from '../reducers/appReducers';
+import ConnectedWeatherContainer from './WeatherContainer';
 import Weather from '../components/Weather';
-import {configure, mount, render, shallow} from 'enzyme';
+import {configure, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
 
-describe('<WeatherContainer/>', () => {
-    let props;
 
-    beforeEach(() => {
-        props = {
-            fetchWeatherData: jest.fn(() => {}),
-            isLoading: false,
-            currentConditions: {
-                locationName: 'London, UK',
-                dataTimestamp: '21:30',
-                temperature: 26.2,
-                weatherIcon: 'ic-sunny',
-                weatherName: 'Sunny day!',
+const flushPromises = () => new Promise((resolve) => setTimeout(resolve));
+
+
+jest.mock('../services/apiClient', () => ({
+    fetchApiForecast: jest.fn(() => {
+        const data = {
+            current_conditions: {
+                datapoint_hour: "18:50",
+                datapoint_date: "July 16",
+                datapoint_time: "July 16, 18:50",
+                data_point_type: "CURRENT_CONDITIONS",
+                location_name: "London, UK",
+                temperature: 26.27,
+                temperature_min: 0,
+                temperature_max: 0,
+                weather_icon: "wi-cloudy",
+                weather_icon_name: "Clouds",
             },
-            hourlyForecast: [
+            hourly_forecast: [
                 {
-                    data_point_type: "HOURLY_FORECAST",
-                    datapoint_date: "July 16",
                     datapoint_hour: "20:00",
+                    datapoint_date: "July 16",
                     datapoint_time: "July 16, 20:00",
+                    data_point_type: "HOURLY_FORECAST",
                     location_name: "London, UK",
                     temperature: 25.9,
-                    temperature_max: 0,
                     temperature_min: 0,
+                    temperature_max: 0,
                     weather_icon: "wi-day-sunny",
                     weather_icon_name: "Intermittent clouds",
                 }
             ],
-            dailyForecast: [
+            daily_forecast: [
                 {
-                    data_point_type: "DAILY_FORECAST",
-                    datapoint_date: "July 17",
                     datapoint_hour: "00:00",
+                    datapoint_date: "July 17",
                     datapoint_time: "July 17, 00:00",
+                    data_point_type: "DAILY_FORECAST",
                     location_name: "London, UK",
                     temperature: 0,
-                    temperature_max: 24,
                     temperature_min: 15,
+                    temperature_max: 24,
                     weather_icon: "wi-cloudy",
                     weather_icon_name: "Heavy Cloud",
                 }
-            ],
-            navigationItems: [
-                {title: "Weather", path: "/"},
-                {title: "About", path: "/about/"}
             ]
         };
-    });
+        return Promise.resolve(data);
+    }),
+}));
 
-    it('renders WeatherContainer properly', () => {
-        const wrapper = shallow(<WeatherContainer {...props}/>);
-        // Expect one navigation
-        expect(wrapper.find('Navigation').length).toBe(1);
 
-        const navigation = wrapper.find('Navigation');
-        expect(navigation.prop('onRefreshClick')).toBe(props.fetchWeatherData);
-        expect(navigation.prop('navigationItems')).toEqual(props.navigationItems);
+let wrapper;
 
-        const weather = wrapper.find('Weather');
-        expect(weather.prop('currentConditions')).toEqual(props.currentConditions);
-        expect(weather.prop('hourlyForecast')).toEqual(props.hourlyForecast);
-        expect(weather.prop('dailyForecast')).toEqual(props.dailyForecast);
-        expect(weather.prop('isLoading')).toBe(false);
-    });
+const setUp = async (initialEntries = ['/']) => {
+    const initialState = {
+        data: {
+            isLoading: true,
+            currentConditions: {},
+            hourlyForecast: [],
+            dailyForecast: [],
+        },
+        navigationItems: [
+            {title: "Weather", path: "/"},
+            {title: "About", path: "/about/"}
+        ]
+    };
 
-    it('routes Weather page properly', () => {
-        const wrapper = mount(
-            <MemoryRouter initialEntries={['/']}>
-              <WeatherContainer {...props}/>
+    const store = createStore(
+        appReducers,
+        initialState,
+        compose(applyMiddleware(...[thunk]))
+    );
+
+    wrapper = mount(
+        <Provider store={store}>
+            <MemoryRouter initialEntries={initialEntries}>
+                <ConnectedWeatherContainer />
             </MemoryRouter>
-        );
-        expect(wrapper.find('AboutPage')).toHaveLength(0);
-        expect(wrapper.find('Weather')).toHaveLength(1);
+        </Provider>
+    );
+
+    await flushPromises();
+    wrapper.update();
+};
+
+describe('e2e tests for the WeatherContainer', () => {
+
+    afterEach(() => {
+        wrapper.unmount();
     });
 
-    it('routes About page properly', () => {
-        const wrapper = mount(
-            <MemoryRouter initialEntries={['/about']}>
-              <WeatherContainer {...props}/>
-            </MemoryRouter>
-        );
-        expect(wrapper.find('Weather')).toHaveLength(0);
-        expect(wrapper.find('AboutPage')).toHaveLength(1);
-    });
+    it('should render all the weather data', async () => {
+        await setUp();
 
-    it('loads the initial data', () => {
-        mount(
-            <MemoryRouter initialEntries={['/']}>
-              <WeatherContainer {...props}/>
-            </MemoryRouter>
-        );
-        expect(props.fetchWeatherData).toHaveBeenCalled();
-    });
-
-    it('renders the app properly', () => {
-        const renderedContainer = render(
-            <MemoryRouter initialEntries={['/']}>
-              <WeatherContainer {...props}/>
-            </MemoryRouter>
-        );
         // Check current weather data
-        expect(renderedContainer.find('.weather__location').text()).toBe(props.currentConditions.locationName);
-        expect(renderedContainer.find('.weather__date').text()).toBe(props.currentConditions.dataTimestamp);
-        expect(renderedContainer.find('.weather__temperature').text()).toBe(`${props.currentConditions.temperature}°C`);
-        expect(renderedContainer.find('.weather__icon.ic-sunny').length).toBe(1);
-        expect(renderedContainer.find('.weather__icon-name').text()).toBe(props.currentConditions.weatherName);
+        const weatherContainer = wrapper.find('#current-weather');
+        expect(weatherContainer.find('.weather__location').text()).toBe('London, UK');
+        expect(weatherContainer.find('.weather__date').text()).toBe('July 16, 18:50');
+        expect(weatherContainer.find('.weather__temperature').text()).toBe('26.27°C');
+        expect(weatherContainer.find('.weather__icon.wi-cloudy')).toHaveLength(1);
+        expect(weatherContainer.find('.weather__icon-name').text()).toBe('Clouds');
 
-        // Expect 1 hourly forecast datapoint
-        const hourlyForecast = renderedContainer.find('.forecast__hour');
-        expect(hourlyForecast.find('.forecast__top-value-1').text()).toBe(`${props.hourlyForecast[0].temperature}°C`);
+        // Check hourly forecast
+        const hourlyForecast = wrapper.find('#hourly-forecast');
+        expect(hourlyForecast.find('.forecast__top-value-1').text()).toBe('25.9°C');
         expect(hourlyForecast.find('.forecast__top-value-2').text()).toBe('');
-        expect(hourlyForecast.find('.weather__icon.wi-day-sunny').length).toBe(1);
-        expect(hourlyForecast.find('.forecast__bottom-value').text()).toBe(props.hourlyForecast[0].datapoint_hour);
+        expect(hourlyForecast.find('.weather__icon.wi-day-sunny')).toHaveLength(1);
+        expect(hourlyForecast.find('.forecast__bottom-value').text()).toBe('20:00');
 
-        // Expect 1 daily forecast datapoint
-        const dailyForecast = renderedContainer.find('.forecast__date');
-        expect(dailyForecast.find('.forecast__top-value-1').text()).toBe(`Max: ${props.dailyForecast[0].temperature_max}°C`);
-        expect(dailyForecast.find('.forecast__top-value-2').text()).toBe(`Min: ${props.dailyForecast[0].temperature_min}°C`);
-        expect(dailyForecast.find('.weather__icon.wi-cloudy').length).toBe(1);
-        expect(dailyForecast.find('.forecast__bottom-value').text()).toBe(props.dailyForecast[0].datapoint_date);
+        // Check daily forecast
+        const dailyForecast = wrapper.find('#daily-forecast');
+        expect(dailyForecast.find('.forecast__top-value-1').text()).toBe('Max: 24°C');
+        expect(dailyForecast.find('.forecast__top-value-2').text()).toBe('Min: 15°C');
+        expect(dailyForecast.find('.weather__icon.wi-cloudy')).toHaveLength(1);
+        expect(dailyForecast.find('.forecast__bottom-value').text()).toBe('July 17');
+    });
+
+    it('should renders the about page properly', async () => {
+        await setUp(['/about']);
+
+        expect(wrapper.find('h1').text()).toBe('About');
     });
 });
